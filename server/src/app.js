811a894cab0bae2dec282244cebdd93d9e70d9a7
oaulyxx <jirayuw64@nu.ac.th>
2023-08-09 16:39:25 +0700

    go express

---
+++ server/src/app.js
@@ -0,0 +1,26 @@
+let express = require('express')
+
+const app = express()
+
+ 
+
+app.get('/status', function (req,res){
+
+    res.send('Hello nodejs server nodemon')
+
+})
+
+
+app.get('/hello/:name', function (req,res) {
+    console.log('hello - ' + req.params.name)
+    res.send('sey hello with ' + req.params.name)
+})
+ 
+
+let port = 8081
+
+app.listen(port, function(){
+
+    console.log('server running on ' + port)
+
+})
