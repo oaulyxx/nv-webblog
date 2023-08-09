4b612aac99f454676d251a0572bdd4c307a3bfa3
oaulyxx <jirayuw64@nu.ac.th>
2023-08-09 16:44:36 +0700

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -15,7 +15,31 @@ app.get('/hello/:name', function (req,res) {
     console.log('hello - ' + req.params.name)
     res.send('sey hello with ' + req.params.name)
 })
+let express = require('express')
+
+const app = express()
+
  
+
+app.get('/status', function (req,res){
+
+    res.send('Hello nodejs server nodemon')
+
+})
+
+
+app.get('/hello/:name', function (req,res) {
+    console.log('hello - ' + req.params.person)
+    res.send('sey hello with ' + req.params.person)
+})
+ 
+
+app.listen(port, function(){
+
+    console.log('server running on ' + port)
+
+})
+
 
 let port = 8081
 
